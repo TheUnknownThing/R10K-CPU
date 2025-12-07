28baffa8f4314c19fc397fa1a439dc27753efca3
TheUnknownThing <wanghn051@outlook.com>
2025-12-07 20:55:36 +0800

    fix: Refactor LSU module to remove unused 'committed' port and simplify active list handling

--- r10k_cpu/modules/lsu.py
+++ r10k_cpu/modules/lsu.py
@@ -6,19 +6,19 @@ class LSU(Module):
     """Performs load and store operations."""
 
     def __init__(self):
-        super().__init__(ports={"instr": LSQEntryType, "committed": Bits(1)})
+        super().__init__(ports={"instr": LSQEntryType})
         self.name = "LSU"
     
     @module.combinational
     def build(self, physical_register_file: Array, memory: SRAM, wb: Module):
-        instr, committed = self.pop_all_ports(False)
+        instr = self.pop_all_ports(False)
         
-        store_active = (instr.is_store & instr.valid & committed).bitcast(Bits(1)) # store only when committed
+        store_active = (instr.is_store & instr.valid).bitcast(Bits(1)) # store only when committed
         load_active = (instr.is_load & instr.valid).bitcast(Bits(1))
-        need_update_active_list = (instr.is_load & instr.valid).bitcast(Bits(1)) | (instr.is_store & instr.valid & ~committed).bitcast(Bits(1))
+        need_update_active_list = load_active # store instruction always has ready bit.
 
         addr = (physical_register_file[instr.rs1_physical].bitcast(Int(32)) + instr.imm.bitcast(Int(32))).bitcast(Bits(32))
-        val = instr.is_store.select(physical_register_file[instr.rs2_physical], Bits(32)(0))
+        val = store_active.select(physical_register_file[instr.rs2_physical], Bits(32)(0))
 
         # YEAH... Assassyn do not support writing a half word or a byte yet...
         # So we just write a full word always.
